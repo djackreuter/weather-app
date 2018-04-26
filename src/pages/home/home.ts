09b7428e01bfb8e5ec4ccf0cdd766446f4e2c9da
Jack Reuter <djreuter45@gmail.com>
2018-04-26 14:18:33 -0600

    injecting weather provider into component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // inject weather component as dependancy
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
 
   }
 
