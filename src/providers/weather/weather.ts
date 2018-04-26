96aeff5981577953e0996dc4f6f14a9f82d9b00b
Jack Reuter <djreuter45@gmail.com>
2018-04-26 14:33:24 -0600

    added weather provider (service)

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class WeatherProvider {
+  apiKey = '';
+  url;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  // returns observable for component to subscribe to
+  getWeather(city, state) {
+    return this.http.get(this.url + '/' + state + '/' + city + '.json')
+    .map((res: Response) => res.json());
+  }
+
+}
